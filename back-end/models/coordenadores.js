ff54655f992d61f38ab8e18918c8f9769426ae5b
atila fonseca <eng.atila.sete@gmail.com>
2024-06-13 16:32:00 -0300

    continuando o plano B

---
+++ back-end/models/coordenadores.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const coordSchema = new mongoose.Schema({
+    siape: {
+        type: String, 
+        required: true, 
+    },
+    email: {
+        type: String, 
+        required: true, 
+    },
+    telefone: {
+        type: String, 
+        required: true, 
+    },
+    senha: {
+        type: String, 
+        required: true, 
+    },
+    created: {
+        type: Date, 
+        required: true, 
+        default: Date.now,
+    }
+});
+module.exports = mongoose.model('Coordenador', coordSchema);
--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
 
 //DB_URL = mongodb://localhost:27017/db_jifs
 
@@ -13,6 +15,32 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'front-end')));
+
+
+app.use(session({
+    secret:'minha chave secreta',
+    saveUninitialized: true,
+    resave: false, 
+}));
+
+
+app.use((req, res, next)=> {
+    res.locals.message = req.session.message; 
+    delete req.session.message; 
+    next();
+})
+
+
+//comunicacao com banco de dados
+//conexão com o db
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection; 
+db.on("error", (error)=> {
+    console.log(error);
+});
+db.once("open", ()=> {
+    console.log("banco de dados conectado!");
+})
 
 //atualizar o projeto no github sem atualizar no vercel, como fazer isso. 
 
