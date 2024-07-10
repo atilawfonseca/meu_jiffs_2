cffe679878250ec135bf8cd9cca6166e3835ac9e
atila fonseca <eng.atila.sete@gmail.com>
2024-07-10 08:53:02 -0300

    erro ao inserir dados no banco de dados

--- rotas/rotas_views.js
+++ rotas/rotas_views.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rotas = express.Router();
+const Coordenador = require('../back-end/models/coordenadores');
 
 
 rotas.get('/', async (req, res) => {
@@ -36,6 +37,7 @@ rotas.get('/login', async (req, res) => {
 })
 
 rotas.get('/cadastrar', async (req, res) => {
+
     try{
         res.render('cadastro', {
             title: 'cadastro'
@@ -44,6 +46,34 @@ rotas.get('/cadastrar', async (req, res) => {
     catch(error){
         res.json({message: error.message})
     }
+})
+
+rotas.post('/add', async (req, res) => {
+    const coordenador = new Coordenador({
+        siape: req.body.siape_coordenador, 
+        email: req.body.email,
+        telefone: req.body.telefone,
+        senha: req.body.password
+    })
+    
+    try {
+        console.log(req.body.siape);
+        console.log(req.body.email);
+        console.log(req.body.telefone);
+        console.log(req.body.password);
+        req.session.message = {
+            type:"success",
+            message: "Coordenador cadastrado com sucesso!" 
+        }
+
+        res.redirect('/');
+    }
+    catch (error) {
+        console.error("Erro ao salvar os dados: ", error);
+        res.json({message: error.message, type: "danger"})
+    }
+    
+    
 })
 
 rotas.get('/recuperar_senha', async (req, res) => {
